2d8c9c7d951f317ab66d959b16f727ab2d50b3bb
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-24 08:29:57 -0300

    finalizando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Link, Outlet } from "react-router-dom"
+
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+
+  return (
+    <div className="app">
+      <Navbar />
+      
+      <Outlet />
+    </div>
+
+  )
+}
+
+export default App
